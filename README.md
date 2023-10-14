67cd7bc3ab447069dce3b0b63f2303fa0c0e6f16
Santiago Taracena <tar20017@uvg.edu.gt>
2023-10-13 18:58:23 -0600

    Done

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # ssl-mpi-decrypter
+
+Este proyecto se centra en la búsqueda y recuperación de llaves de cifrado DES (Estándar de Cifrado de Datos) utilizando diferentes enfoques y estrategias de cómputo paralelo. El objetivo es evaluar la eficiencia de varias técnicas y comparar sus ventajas y desventajas para la búsqueda de llaves de cifrado DES en un entorno distribuido.
+
+## Miembros del Equipo
+
+- José Rodrigo Barrera García (20807)
+- Marco Pablo Orozco Saravia (20857)
+- Santiago Taracena Puga (20017)
+
+## Descripción del Proyecto
+
+El proyecto se divide en varias etapas, cada una de las cuales se enfoca en una estrategia diferente para buscar llaves de cifrado DES. Las estrategias incluyen:
+
+1. **Enfoque Naive**: Un enfoque sencillo de fuerza bruta que prueba cada llave en un rango dado.
+2. **Enfoque de Arriba hacia Abajo**: Una estrategia optimizada que inicia la búsqueda desde la llave máxima y se desplaza hacia abajo.
+3. **Enfoque de Dos Llaves a la Vez**: Una estrategia teóricamente eficiente que revisa dos llaves adyacentes simultáneamente.
+
+## Implementación
+
+El proyecto está implementado en lenguaje C y hace uso de las siguientes tecnologías y bibliotecas:
+
+- Código en C para la manipulación de llaves DES y el cómputo paralelo.
+- Biblioteca OpenSSL para operaciones de cifrado y descifrado DES.
+- MPI (Interfaz de Paso de Mensajes) para la programación paralela y distribuida.
+
+## Instrucciones de Uso
+
+1. Asegúrate de tener instalado MPI y la biblioteca OpenSSL en tu sistema.
+
+2. Compila el código fuente utilizando el compilador C. Puedes usar el siguiente comando:
+
+```bash
+mpicc -o main main.c -lcrypto
+```
+
+Ejecuta el programa MPI con el número de nodos deseados. Por ejemplo, para ejecutar con 4 nodos:
+
+```bash
+mpirun -np 4 ./main <archivo-de-entrada> <llave-privada> <archivo-a-buscar>
+```
+
+El programa se encargará de iniciar la búsqueda de la llave y proporcionará resultados una vez encontrada.
+
+## Resultados
+
+Después de ejecutar el programa con diferentes enfoques y estrategias, se recopilaron datos y se realizaron análisis comparativos. Los resultados se detallan en el informe del proyecto adjunto.
+
+## Contribuciones
+
+Siéntete libre de contribuir o mejorar este proyecto. Puedes hacerlo a través de bifurcaciones (forks) y solicitudes de extracción (pull requests).
+
+## Licencia
+
+Este proyecto se publica bajo la Licencia MIT. Consulta el archivo LICENSE para obtener más detalles.
+
+Este proyecto se realizó como parte de un curso universitario (CC3069 - Computación Paralela y Distribuida) en la Universidad del Valle de Guatemala.
+
+Por supuesto, asegúrate de personalizar el archivo `README.md` con información específica de tu proyecto, como los resultados reales que obtuviste y cualquier detalle adicional que desees compartir con los usuarios y colaboradores potenciales.
---
+++ src/app-01-par.c
@@ -0,0 +1,282 @@
+/**
+ * Universidad del Valle de Guatemala
+ * (CC3069) Computación Paralela y Distrubida
+ * Proyecto 2 - Memoria Distribuida
+ * 
+ * Miembros del equipo de trabajo:
+ * - José Rodrigo Barrera García (20807)
+ * - Marco Pablo Orozco Saravia (20857)
+ * - Santiago Taracena Puga (20017)
+ */
+
+// Librerías requeridas para el proyecto.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <openssl/des.h>
+#include <mpi.h>
+
+// Función que encripta un mensaje.
+void encrypt(char* source, char* destiny, DES_key_schedule schedule) {
+
+  // Ciclo que encripta el texto completo en cadenas de 8 bytes.
+  for (int i = 0; i < strlen(source); i += 8) {
+
+    // Array con el texto de 8 bytes inicial original.
+    char originalSentence[8] = { source[i], source[(i + 1)], source[(i + 2)], source[(i + 3)], source[(i + 4)], source[(i + 5)], source[(i + 6)], source[(i + 7)] };
+
+    // Array temporal para colocar el resultado encriptado.
+    char encryptedSentence[8] = { "" };
+
+    // Proceso de encriptado de la oración.
+    DES_ecb_encrypt((const_DES_cblock*)originalSentence, (DES_cblock*)encryptedSentence, &schedule, DES_ENCRYPT);
+
+    // Mapping del resultado encriptado al destino.
+    for (int j = 0; j < 8; j++) {
+      destiny[(i + j)] = encryptedSentence[j];
+    }
+  }
+}
+
+// Función que desencripta un mensaje.
+void decrypt(char* source, char* destiny, DES_key_schedule schedule) {
+
+  // Ciclo que desencripta el texto completo en cadenas de 8 bytes.
+  for (int i = 0; i < strlen(source); i += 8) {
+
+    // Array con el texto de 8 bytes inicial encriptado.
+    char encryptedSentence[8] = { source[i], source[(i + 1)], source[(i + 2)], source[(i + 3)], source[(i + 4)], source[(i + 5)], source[(i + 6)], source[(i + 7)] };
+
+    // Array temporal para colocar el resultado desencriptado.
+    char decryptedSentence[8] = { "" };
+
+    // Proceso de desencriptado de la oración.
+    DES_ecb_encrypt((const_DES_cblock*)encryptedSentence, (DES_cblock*)decryptedSentence, &schedule, DES_DECRYPT);
+
+    // Mapping del resultado desencriptado al destino.
+    for (int j = 0; j < 8; j++) {
+      destiny[(i + j)] = decryptedSentence[j];
+    }
+  }
+}
+
+// Función que prueba una llave.
+int tryKey(long keyToTest, char* source, char* searchedText) {
+
+  // Llave en formato de string.
+  char stringKey[256];
+  sprintf(stringKey, "%ld", keyToTest);
+
+  // Variables de DES temporales.
+  DES_cblock temporalKeyToTest;
+  DES_key_schedule temporalSchedule;
+
+  // Configuración de la llave temporal.
+  DES_string_to_key(stringKey, &temporalKeyToTest);
+  DES_set_key((const_DES_cblock*)&temporalKeyToTest, &temporalSchedule); 
+
+  // Instancia del texto temporal.
+  char temporalText[strlen(source)];
+
+  // Desencriptación del texto con la llave a probar.
+  decrypt(source, temporalText, temporalSchedule);
+
+  // Retorno si el texto encontrado es correcto.
+  return (strstr((char*)temporalText, searchedText) != NULL);
+}
+
+// Función main, que ejecuta el código del programa.
+int main(int argc, char* argv[]) {
+
+  // Verificación de los argumentos pasados en consola.
+  if (argc < 4) {
+    perror("Uso del programa: ./naive-par <input-file> <private-key> <searched-file>\n");
+    return EXIT_FAILURE;
+  }
+
+  /**
+   * Llaves probadas:
+   * - 123456
+   * - 1234567
+   * - 12345678
+   * - 18014398509481983 (2 ** 56 / 4)
+   * - 18014398509481984 (2 ** 56 / 4 + 1)
+   */
+
+  // Path al archivo a usar y llave privada usada.
+  char* filePath = argv[1];
+  char* inputKey = argv[2];
+  char* searchedTextPath = argv[3];
+
+  // Threads a utilizar y rank de cada uno.
+  int threads, rank;
+
+  // Upper bound de la búsqueda, es decir, el límite de las llaves.
+  long upperBound = (1L << 56); 
+
+  // Instancia del archivo leído.
+  FILE* inputFile = fopen(filePath, "rb");
+
+  // Verificación de la existencia del archivo leído.
+  if (!inputFile) {
+    perror("Error abriendo el archivo.\n");
+    return EXIT_FAILURE;
+  }
+
+  // Obtención de la longitud del archivo.
+  fseek(inputFile, 0, SEEK_END);
+  long fileLength = ftell(inputFile);
+  rewind(inputFile);
+
+  // Asignación de memoria para el archivo.
+  char* inputText = (char*)malloc(fileLength);
+
+  // Verificación de la asignación de memoria.
+  if (!inputText) {
+    perror("Error asignando memoria para el archivo.\n");
+    fclose(inputFile);
+    return EXIT_FAILURE;
+  }
+
+  // Lectura y almacenamiento del archivo.
+  if (fread(inputText, 1, fileLength, inputFile) != fileLength) {
+    perror("Error leyendo el archivo.\n");
+    free(inputText);
+    fclose(inputFile);
+    return EXIT_FAILURE;
+  }
+
+  // Cierre del archivo de entrada.
+  fclose(inputFile);
+
+  // Instancia del archivo con texto a buscar.
+  FILE* searchedTextFile = fopen(searchedTextPath, "rb");
+
+  // Verificación de la existencia del archivo leído.
+  if (!searchedTextFile) {
+    perror("Error abriendo el archivo.\n");
+    return EXIT_FAILURE;
+  }
+
+  // Obtención de la longitud del archivo.
+  fseek(searchedTextFile, 0, SEEK_END);
+  long searchedFileLength = ftell(searchedTextFile);
+  rewind(searchedTextFile);
+
+  // Asignación de memoria para el archivo.
+  char* searchedText = (char*)malloc(searchedFileLength);
+
+  // Verificación de la asignación de memoria.
+  if (!searchedText) {
+    perror("Error asignando memoria para el archivo.\n");
+    fclose(searchedTextFile);
+    return EXIT_FAILURE;
+  }
+
+  // Lectura y almacenamiento del archivo.
+  if (fread(searchedText, 1, searchedFileLength, searchedTextFile) != searchedFileLength) {
+    perror("Error leyendo el archivo.\n");
+    free(searchedText);
+    fclose(searchedTextFile);
+    return EXIT_FAILURE;
+  }
+
+  // Cierre del archivo del texto a buscar.
+  fclose(searchedTextFile);
+
+  // Llave y calendarización de DES.
+  DES_cblock key;
+  DES_key_schedule schedule;
+
+  // Configuración de la llave.
+  DES_string_to_key(inputKey, &key);
+  DES_set_key((const_DES_cblock*)&key, &schedule); 
+
+  // Instancia del texto encrpitado y desencriptado.
+  char encryptedText[strlen(inputText)];
+  char decryptedText[strlen(inputText)];
+
+  // Función para encriptar el texto.
+  encrypt(inputText, encryptedText, schedule);
+
+  // Inicialización de MPI.
+  MPI_Init(NULL, NULL);
+
+  // Request y Status de MPI.
+  MPI_Status status;
+  MPI_Request request;
+
+  // Obtención de los threads y el rank de cada uno.
+  MPI_Comm_size(MPI_COMM_WORLD, &threads);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Llave a encontrar.
+  long foundKey = 0L;
+  int keyHasBeenFound = 0;
+
+  // Distribución del trabajo de forma naive.
+  long boundPerThread = (upperBound / threads);
+  long localLowerBound = (boundPerThread * rank);
+  long localUpperBound = ((boundPerThread * (rank + 1)) - 1);
+
+  // Asignación del límite máximo para el último thread.
+  if (rank == (threads - 1)) {
+    localUpperBound = upperBound;
+  }
+
+  // Non blocking receive, revisión en el for si alguien ya encontró la llave.
+  MPI_Irecv(&foundKey, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+
+  // Ciclo for que busca la llave en el límite dado.
+  for (long i = localUpperBound; i > localLowerBound; i--) {
+
+    // Testing que verifica la llave encontrada.
+    MPI_Test(&request, &keyHasBeenFound, MPI_STATUS_IGNORE);
+
+    // Finalización del ciclo si la llave es encontrada por otro proceso.
+    if (keyHasBeenFound) break;
+
+    // Verificación de la llave.
+    if (tryKey(i, encryptedText, searchedText)) {
+
+      // Intercambio con la llave encontrada.
+      foundKey = i;
+
+      // Impresión del resultado.
+      printf("El proceso %d ha encontrado la llave: %ld\n", rank, foundKey);
+
+      // Mensaje avisando a los demás nodos que la llave se encontró.
+      for (int node = 0; node < threads; node++) {
+        MPI_Send(&foundKey, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
+      }
+
+      // Salida del ciclo.
+      break;
+    }
+  }
+
+  // Aviso de la llave encontrada.
+  if (rank == 0) {
+
+    // Espera a la finalización de todo.
+    MPI_Wait(&request, &status);
+
+    // Cambio del schedule para la nueva llave.
+    DES_key_schedule foundKeySchedule;
+    DES_set_key_unchecked((DES_cblock*)&foundKey, &foundKeySchedule);
+
+    // Desencripción con la llave encontrada.
+    decrypt(encryptedText, decryptedText, schedule);
+
+    // Impresión de los resultados.
+    printf("Texto original: %s\n", inputText);
+    printf("Texto cifrado: %s\n", encryptedText);
+    decryptedText[(int)strlen(decryptedText) - (int)strlen(encryptedText)] = '\0';
+    printf("Texto descifrado: %s\n", decryptedText);
+  }
+
+  // Salida correcta del programa.
+  MPI_Finalize();
+  return EXIT_SUCCESS;
+}
